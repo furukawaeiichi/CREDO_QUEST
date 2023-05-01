e2d4339177e4761a2acfc08977b60871de32705b
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-02 00:39:11 +0900

    ページ遷移時に非同期通信でbgmが途切れないようにできた！！！

--- app/javascript/custom/bgm.js
+++ app/javascript/custom/bgm.js
@@ -1,4 +1,24 @@
 document.addEventListener("turbo:load", () => {
   const bgm = document.querySelector("#bgm");
-  bgm?.play();
+
+  // 保存されている再生状態を取得
+  const wasPlaying = sessionStorage.getItem("wasPlaying") === "true";
+
+  // 保存されている再生状態に基づいてBGMを再生または一時停止
+  if (bgm) {
+    if (wasPlaying) {
+      bgm.play();
+    } else {
+      bgm.pause();
+    }
+  }
+
+  // 再生と一時停止のイベントリスナーを追加し、再生状態を保存
+  bgm.onplay = () => {
+    sessionStorage.setItem("wasPlaying", "true");
+  };
+
+  bgm.onpause = () => {
+    sessionStorage.setItem("wasPlaying", "false");
+  };
 });
