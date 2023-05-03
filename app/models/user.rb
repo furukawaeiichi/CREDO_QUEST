3a2d9c9ae119c2ff8760310d7c28208671fd329f
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-03 17:02:11 +0900

    model作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,16 @@
 class User < ApplicationRecord
+  # 引数として認証情報(auth)を受け取り、
+  def self.find_or_create_from_auth(auth)
+    uid = auth[:uid]
+    nickname = auth[:info][:nickname]
+    name = auth[:info][:name]
+    image = auth[:info][:image]
+
+    # uidが一致するUserを検索、なければ作成
+    self.find_or_create_by(uid: uid) do |user|
+      user.name = auth[:info][:name]
+      user.nickname = auth[:info][:nickname]
+      user.image = auth[:info][:image]
+    end
+  end
 end
