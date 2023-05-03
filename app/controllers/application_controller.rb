b3bb768647bb9650fe53ecffaa41183ba0e8eacc
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-03 17:39:06 +0900

    app/controllers/application_controller.rb作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,4 @@
 class ApplicationController < ActionController::Base
+  include SessionsHelper  # module SessionsHelperが全てのコントローラーで使えるようになる
+  protect_from_forgery with: :exception # CSRF攻撃から保護する
 end
