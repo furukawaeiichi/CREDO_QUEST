8b9bf94dac9b7ecba6cc22fc3f8d88cb8be19e08
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-04 19:09:34 +0900

    model作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,15 @@
 class User < ApplicationRecord
+  def self.find_or_create_from_auth(auth)
+    provider = auth[:provider]
+    uid = auth[:uid]
+    nickname = auth[:info][:nickname]
+    name = auth[:info][:name]
+    image = auth[:info][:image]
+
+    self.find_or_create_by(provider: provider, uid: uid) do |user|
+      user.nickname = auth[:info][:nickname]
+      user.name = auth[:info][:name]
+      user.image = auth[:info][:image]
+    end
+  end
 end
