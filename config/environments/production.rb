fb9f0fd9305b026d54b0d95698a39224e2675e4d
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-04 18:09:31 +0900

    database名をCSS_TESTからCREDO_QUESTに変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,7 +60,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "CSS_TEST_production"
+  # config.active_job.queue_name_prefix = "CREDO_QUEST_production"
 
   config.action_mailer.perform_caching = false
 
