ab34746e46f169c876fe8dbf257747cb8d810bfe
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-04 19:14:03 +0900

    routes設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,10 @@ Rails.application.routes.draw do
 
   get 'home/top', to: 'home#top'
   get 'home/special_thanks', to: 'home#special_thanks'
+
+  get "/auth/:provider/callback", to: "sessions#create"
+  get "/auth/failure", to: "sessions#failure"
+  delete "/sign_out", to: "sessions#destroy"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
