81957bd51e309d819bdeaca9a47eff72f424b972
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-05 14:16:07 +0900

    デプロイ環境でもTwitterログインに成功！！！

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,6 @@ if Rails.application.credentials.twitter
             Rails.application.credentials.twitter[:client_secret],
             callback_path: "/auth/twitter2/callback",
             scope: "tweet.read users.read"
-
     # 認証失敗したら、/auth/failure にリダイレクト
     OmniAuth.config.on_failure =
       Proc.new { |env| OmniAuth::FailureEndpoint.new(env).redirect_to_failure }
