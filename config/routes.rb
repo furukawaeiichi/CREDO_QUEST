ee6040afb73b0cacc681536f41a7ae7fb8b70504
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-05 17:39:30 +0900

    パスを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,17 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#top"
 
-  get "home/top", to: "home#top"
-  get "home/special_thanks", to: "home#special_thanks"
+  # Prefix Verb     URI Pattern     Controller#Action
+  # HTTPリクエストメソッド      "パス",      to: "コントローラー名#アクション名"
+
+  get "/home/top", to: "home#top"
+  get "/home/special_thanks", to: "home#special_thanks"
 
   get "/auth/twitter2/callback", to: "sessions#create"
   get "/auth/failure", to: "sessions#failure"
   delete "/sign_out", to: "sessions#destroy"
+
+  get "/users", to: "users#show"
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 end
