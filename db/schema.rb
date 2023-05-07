0d28023f1903a326ce84d5b4670b7c22020a0b0b
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-07 17:21:23 +0900

    データベースに反映

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_05_04_100152) do
+ActiveRecord::Schema[7.0].define(version: 2023_05_07_081836) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "lists", force: :cascade do |t|
+    t.string "title"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "todos", force: :cascade do |t|
+    t.string "content"
+    t.boolean "checked"
+    t.bigint "list_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["list_id"], name: "index_todos_on_list_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "provider"
@@ -24,4 +41,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_04_100152) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "lists", "users"
+  add_foreign_key "todos", "lists"
 end
