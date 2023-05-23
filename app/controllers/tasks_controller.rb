9dbbc1dfffdf3fb3c6371a09e749c3553cdc79a8
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-23 16:50:21 +0900

    destroyのレスポンスに status: :see_other を付ける

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,7 +55,7 @@ class TasksController < ApplicationController
     @task.destroy
 
     respond_to do |format|
-      format.html { redirect_to tasks_url, notice: "クエストが削除されました！" }
+      format.html { redirect_to tasks_url, notice: "クエストが削除されました！", status: :see_other }
       format.json { head :no_content }
     end
   end
