fe689dbf33ebc8270d55d015730a04a80f284e80
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-05-28 02:40:10 +0900

    task/indexに遷移せず、user/showでtask CRUDを実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])                # params[:id]でユーザーを探す
+    @task = Task.new                              # 新しいタスクを作成
     @user_tasks = @user.tasks.order(id: :desc)    # ユーザーのタスクを新しい順に並べる
   end
 end
