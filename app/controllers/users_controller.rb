2516bff5ab2c422f7075ed5879c35f480fcb0821
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-06-05 21:46:07 +0900

    UsersControllerにbefore_action追記

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!               # サインインしていなければ、root_pathにリダイレクト
+  before_action :logged_in_user, only: [:index, :show]
+  before_action :correct_user,   only: [:edit, :update, :destroy]
 
   def show
     @user = User.find(params[:id])                # params[:id]でユーザーを探す
