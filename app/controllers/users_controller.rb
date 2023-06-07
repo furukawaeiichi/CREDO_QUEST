3dbd49628c7de28895fb5d28678998af467f468a
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-06-07 18:16:40 +0900

    controllerでtask/formにcurrent_user規制

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,8 +7,10 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])                # params[:id]でユーザーを探す
+    if current_user == @user                      # ログインしているユーザーと、表示しているユーザーが同じなら
+      @task = Task.new                            # 新しいタスクを作成
+    end
     @lists = @user.lists
-    @task = Task.new                              # 新しいタスクを作成
     @user_tasks = @user.tasks.order(id: :desc)    # ユーザーのタスクを新しい順に並べる
   end
 
