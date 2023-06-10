e72fbf56ee3ea283029b85c2905ccc881c503a39
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-06-10 23:55:09 +0900

    gem jquery-railsをbundle install

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,7 @@ import "@hotwired/turbo-rails";
 import "controllers";
 
 import "./custom/bgm";
+
+//= require jquery3
+//= require popper
+//= require bootstrap-sprockets
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,6 @@ pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin_all_from "app/javascript/controllers", under: "controllers"
 
 pin_all_from 'app/javascript/custom', under: 'custom'
+
+pin "bootstrap", to: "bootstrap.min.js", preload: true
+pin "@popperjs/core", to: "popper.js", preload: true
