749214fa587043b5a7025f97b00f7e04ada98563
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-06-17 21:26:14 +0900

    progress-barが各title todoのcheck数に比例して表示される

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,27 @@
 module UsersHelper
+  def checked_todos_count(list_title)
+    list = List.find_by(title: list_title)
+    list.todos.where(checked: true).count
+  end
+
+  def list_bar_color(title)
+    case title
+    when 'Be Open'
+      'bg-primary'
+    when 'Move Fast'
+      'bg-success'
+    when 'Give First'
+      'bg-info'
+    when 'Geek Out'
+      'bg-warning'
+    when 'Take Ownership'
+      'bg-danger'
+    else
+      'bg-secondary'
+    end
+  end
+
+  def progress_percentage(list)
+    (checked_todos_count(list.title).to_f / list.todos.count.to_f) * 100
+  end
 end
