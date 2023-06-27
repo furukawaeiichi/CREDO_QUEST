e808b5155d842db36ba9e7bff5086d5a039109da
furukawaeiichi <102509805+furukawaeiichi@users.noreply.github.com>
2023-06-27 22:19:48 +0900

    userページネーション実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!               # サインインしていなければ、root_pathにリダイレクト
 
   def index
-    @users = User.all
+    @users = User.page(params[:page])
   end
 
   def show
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_tasks, through: :likes, source: :task
 
+  paginates_per 1
+
   def level
     self.lists.sum { |list| list.todos.where(checked: true).count + list.tasks.where(checked: true).count }
   end
